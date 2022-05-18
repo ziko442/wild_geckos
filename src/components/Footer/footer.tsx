700a48b2999de0069d26323cacb83e326a4eb8a7
ziko442 <ziko442@gmail.com>
2022-05-18 16:55:58 +0100

    update footer

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,23 +1,33 @@
-import  React from 'react';
+import React from "react";
 
-const  Footer = () => {
-    return (
-        <footer className="nk-footer-bar section section-s tc-light">
-            <div className="container container-xxl">
-                <div className="row gutter-vr-10px">
-                    <div className="col-lg-6 order-lg-last text-lg-right">
-                        <ul className="footer-nav">
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Terms of sales</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="copyright-text copyright-text-s2">Copyright © 2019 ICO. Handcrafted by <a href="#">iO</a>.</div>
-                    </div>
-                </div>
+const Footer = () => {
+  return (
+    <footer className="nk-footer-bar section section-s tc-light">
+      <div className="container container-xxl">
+        <div className="row gutter-vr-10px">
+          <div className="col-lg-6 order-lg-last text-lg-right">
+            <ul className="footer-nav">
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Terms of sales</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-6">
+            <div className="copyright-text copyright-text-s2 tc-white">
+              COPYRIGHT © 2022 WLID GECKOS PROJECT{" "}
+              <a href="#" className="tc-white">
+                iO
+              </a>
+              .
             </div>
-        </footer>
-    );
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
